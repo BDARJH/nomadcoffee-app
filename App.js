6fa0952a97848b57829de854c8ec27931e72b5dd
JINHUI KIM <xzdfor@naver.com>
2021-10-19 00:37:18 +0900

    Created a new Expo app and AppLoading

---
+++ App.js
@@ -0,0 +1,47 @@
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [
+      require("./assets/logo.png"),
+      "http://drive.google.com/uc?id=1EsEQPVbnVDpjn45LTd7_QaSkyHKn4OeT",
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <Image resizeMode="contain" source={require("./assets/logo.png")} />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
